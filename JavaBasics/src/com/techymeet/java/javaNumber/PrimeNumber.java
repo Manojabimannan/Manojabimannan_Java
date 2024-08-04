b9a53242dbc01a3b4d677cd4e7ba9ec77bfe0759
Manojabimannan <manojabhimanyuu.techymeet@gmail.com>
2024-08-04 16:19:40 +0530

    Inital Push Code

---
+++ JavaBasics/src/com/techymeet/java/javaNumber/PrimeNumber.java
@@ -0,0 +1,27 @@
+package com.techymeet.java.javaNumber;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+	public static void main(String[] args) {
+
+		Scanner in =new Scanner(System.in);
+		int number=29;
+		Boolean isprime=true;
+		for(int i=2;i<=number/2;i++) {
+			if(number%2==0) {
+				isprime=false;
+			}
+		}
+		System.out.println(" "
+				+ "");
+		if(isprime&&number>1) {
+			System.out.println(number+"PrimeNumber");
+		}else {
+			System.out.println(number+"Not a PrimeNumber");
+		}
+		
+	}
+
+}
