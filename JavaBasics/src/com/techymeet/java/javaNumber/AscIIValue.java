81592cc39b3808bc67d1951abc6e4e2129005c23
Manojabimannan <manojabhimanyuu.techymeet@gmail.com>
2024-08-14 09:27:22 +0530

    Ascii

---
+++ JavaBasics/src/com/techymeet/java/javaNumber/AscIIValue.java
@@ -0,0 +1,12 @@
+package com.techymeet.java.javaNumber;
+
+public class AscIIValue {
+
+	public static void main(String[] args) {
+
+		for(int i=0;i<255;i++) {
+			System.out.println("AscII Value"+" "+(char)i+" "+i);
+		}
+	}
+
+}
